3474986c5061286ce60b2f08f88be02c4cc5fc9f
micahkatz <micahj2110@gmail.com>
2021-07-09 14:11:08 -0500

    added alerts

--- src/funcs/alertFuncs.js
+++ src/funcs/alertFuncs.js
@@ -1,31 +1,31 @@
 import { API } from 'aws-amplify';
 var uuid4 = require('uuid4');
-let apiName = 'AlertsApi'
-let path = '/alerts'
-let alertId = '1234'
+let apiName = 'AlertsApi';
+let path = '/prod/alerts';
+let alertId = '1234';
 export async function NewAlert(type) {
-  var id = uuid4();
-  try {
-    const response = await API.put(apiName, path, {
-      body: {id: alertId, type}
-    });
-  } catch (e) {
-    console.log(e)
-  }
+    var id = uuid4();
+    try {
+        const response = await API.put(apiName, path, {
+            body: { id: alertId, type },
+        });
+    } catch (e) {
+        console.log(e);
+    }
 }
 export async function DelAlert() {
-  try {
-    const response = await API.del(apiName, path + '/object/' + alertId);
-    console.log(response)
-  } catch (e) {
-    console.log(e)
-  }
+    try {
+        const response = await API.del(apiName, path + '/object/' + alertId);
+        console.log(response);
+    } catch (e) {
+        console.log(e);
+    }
 }
 export async function CheckAlerts() {
-  try {
-    const response = await API.get(apiName, path + '/feed');
-    return response
-  } catch (e) {
-    console.log(e)
-  }
+    try {
+        const response = await API.get(apiName, path + '/feed');
+        return response;
+    } catch (e) {
+        console.log(e);
+    }
 }
